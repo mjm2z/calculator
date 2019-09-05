bac2aaefa40247bee6e764367b7746c426ed8cf3
Mike M <mikem@Mikes-MacBook-Pro.local>
2019-09-04 20:44:22 -0400

    chore: faded borders - shifting gears on design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Numbers from "./Components/numbers/Numbers";
+import Calculator from "./Components/calculator/Calculator";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <Numbers />
+      <Calculator />
     </div>
   );
 }
--- src/Components/numbers/Numbers.js
+++ src/Components/numbers/Numbers.js
@@ -3,22 +3,61 @@ import "bootstrap/dist/css/bootstrap.css";
 class Numbers extends Component {
   render() {
     return (
-      <div className='number_container'>
-        <div className='top_number_row number_row'>
-            <button className='number_button'>7</button>
-            <button className='middle_number number_button'>8</button>
-            <button className='number_button'>9</button>
-        </div>
-        <div className='number_row'>
-            <button className='number_button'>4</button>
-            <button className='middle_number number_button'>5</button>
-            <button className='number_button'>6</button>
-        </div>
-        <div className='bottom_number_row number_row'>
-            <button className='number_button'>1</button>
-            <button className='middle_number number_button'>2</button>
-            <button className='number_button'>3</button>
-        </div>
+      <div className="button_container">
+        <table>
+        <tr>
+            <td className="clear_cell">
+              <button className="button">AC</button>
+            </td>
+            <td className="negate_cell">
+            <button className="button">+/-</button>
+            </td>
+            <td className="percentage_cell">
+            <button className="button">%</button>
+            </td>
+          </tr>
+          <tr>
+            <td className="seven_cell">
+              <button className="button">7</button>
+            </td>
+            <td className="eight_cell">
+            <button className="button">8</button>
+            </td>
+            <td className="nine_cell">
+            <button className="button">9</button>
+            </td>
+          </tr>
+          <tr>
+            <td className="four_cell">
+              <button class="button">4</button>
+            </td>
+            <td className="five_cell">
+              <button className="button">5</button>
+            </td>
+            <td className="six_cell">
+              <button className="button">6</button>
+            </td>
+          </tr> 
+          <tr>
+            <td className="one_cell">
+              <button className="button">1</button>
+            </td>
+            <td className="two_cell">
+              <button className="button">2</button>
+            </td>
+            <td className="three_cell">
+              <button className="button">3</button>
+            </td>
+          </tr>
+            <tr>
+            <td className="zero_cell" colSpan="2">
+              <button className="zero_button">0</button>
+            </td>
+            <td class="decimal_cell">
+              <button class="button">.</button>
+            </td>
+          </tr>
+        </table>
       </div>
     );
   }
