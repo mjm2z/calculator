1a73497a057fdf807f9952ad23257cb7ce4a68fb
Mike M <mikem@Mikes-MacBook-Pro.local>
2019-09-15 20:17:06 -0400

    chore: basic redux configuration for button click

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Calculator from "./Components/calculator/Calculator";
 import "./App.css";
-
+import { Provider as ReduxProvider } from 'react-redux'
+import configureStore from './redux';
+const store = configureStore();
 function App() {
   return (
     <div>
+      <ReduxProvider store={store}>
       <Calculator />
+      </ReduxProvider>
+
     </div>
   );
 }
