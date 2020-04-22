e2d28f26a86d62428926caf129edc95a653bb8e1
mjm2z <mjm2z@outlook.com>
2020-04-22 19:43:33 -0400

    fix: changed calculator library, updated display value, setup equals

--- src/Components/buttons/Buttons.js
+++ src/Components/buttons/Buttons.js
@@ -4,7 +4,8 @@ import PropTypes from "prop-types";
 import { actions, selectors } from "../../redux";
 import { connect } from "react-redux";
 import flow from "lodash.flow";
-import stringCalculator from "string-calculator";
+import { evaluate } from "mathjs";
+import { equation } from "../../redux/Reducers";
 
 class Buttons extends Component {
   constructor(props) {
@@ -25,23 +26,98 @@ class Buttons extends Component {
   // Bug at 099=. -> doesn't show 99. shows 0
   // - Maybe put current value in redux to make it easier for negative/positive state
   // Clicking pos/neg causes negative button to highlight
+  // Update readme
 
   handleEquals() {
-    this.props.setEquation(stringCalculator(this.props.equation));
+    let eq = this.props.equation;
+    if(isNaN(eq.charAt(eq.length-1)) && eq.charAt(eq.length-1) !== "." && eq.charAt(eq.length-1) !== ")"){
+      eq = eq.substring(0, eq.length-2)
+    }
+    let result = evaluate(eq + this.getCloseParen());
+    if ( result < 0) {
+      result = `(${result}`
+    }
+
+    this.props.setEquation(result);
 
     // pass equation string into api
   }
+
   handleButtonClick(e) {
     const { equation } = this.props;
+    // Handle AC
     if (e === "AC") {
       this.props.setEquation("0");
-    } else if (isNaN(e) && e !== "." && isNaN(equation[equation.length - 1])) {
-      this.props.setEquation(equation.slice(0, equation.length - 1) + e);
-    } else {
+    }
+    else if (e === "+/-"){
+      this.setSign();
+    }
+    else if (e === "."){
+      this.checkDecimal();
+    }
+    // Handle operations 
+    else if (isNaN(e) && e!=="="){
+      if (isNaN(equation[equation.length - 1])) {
+        this.props.setEquation(equation.slice(0, equation.length - 1) + this.getCloseParen() + e);
+      } else {
+        this.props.setEquation(equation + this.getCloseParen() + e);
+      }
+    }
+
+    // Handle equals or append number 
+    else {
       e === "=" ? this.handleEquals() : this.props.setEquation(equation + e);
     }
 
     // decimal checker
+  }
+  checkDecimal(){
+    let eq = this.props.equation;
+    if (isNaN(eq.charAt(eq.length-1))){
+      this.props.setEquation(eq + "0.");
+    } else {
+      for(let index=eq.length-1; index >= 0; index--) {
+        if(isNaN(eq.charAt(index))){
+          if(eq.charAt(index)!=="."){
+            this.props.setEquation(eq+".");
+          }
+          return;
+        }
+      }
+      this.props.setEquation(eq+".");
+    }
+  }
+  setSign(){
+    let eq = this.props.equation;
+    let index = eq.length-1;
+    for (index; index>0; index--){
+      if(isNaN(eq.charAt(index))){
+        break;
+      }
+    }
+    console.log("Pos/Neg Index: " + index)
+    if (index === 0){
+      this.props.setEquation("(-" + eq)
+    } else if ( index === eq.length -1) {
+      if(eq.charAt(index-1)==="("){
+        this.props.setEquation(eq.substring(0, index-1));
+      } else {
+        this.props.setEquation(eq + "(-")
+      }
+    } else {
+      if(eq.charAt(index-1)==="("){
+        this.props.setEquation(eq.substring(0, index-1)+ eq.substring(index+1));
+      } else {
+        this.props.setEquation(eq.substring(0, index) + eq.charAt(index) + "(-" + eq.substring(index+1) )
+      }
+    }
+
+  }
+  getCloseParen() {
+    const { equation } = this.props;
+    const opCount = (equation.match(/\(/g) || []).length;
+    const clCount = (equation.match(/\)/g) || []).length;
+    return opCount > clCount ? ")" : "";
   }
   getButtonDisplayText(value) {
     switch (value) {
--- src/redux/Reducers/reducers.js
+++ src/redux/Reducers/reducers.js
@@ -1,35 +1,48 @@
 const initialSign = "positive";
 export const currentSign = (state = initialSign, action) => {
   if (action.type === "SIGN") {
+    console.log("Flip sign");
     state = action.getSign();
   }
   return state;
 };
 
 function getDisplayValue(equation) {
+  
+
+// Negative number check (-09)
+// Decimal check 0.
+// Operation check
+
+
   // Have to account for "" "09" "09*" "*09*"
   let eq = equation;
-  if (isNaN(eq.charAt(eq.length - 1))) {
-    eq = eq.substring(0, eq.length - 1);
-  }
-  let index = 0;
-  let currentValue = eq;
-  for (index = eq.length - 1; index > 0; index--) {
-    if (isNaN(eq.charAt(index)) && eq.charAt(index) !== ".") {
-      currentValue = eq.substring(index + 1);
+  let lastNum = eq.length-1;
+  let firstNum = 0;
+  let lastFound = false;
+  for ( let index = eq.length -1; index > 0; index--){
+    if (!lastFound && (!isNaN(eq.charAt(index)) || eq.charAt(index) === ".")){
+      lastNum = index +1;
+      lastFound = true;
+    } else if ( lastFound && isNaN(eq.charAt(index)) && eq.charAt(index) !== "."){
+      firstNum = index +1;
       break;
     }
   }
 
-  return Number(currentValue).toString();
+
+  let currentValue = eq.substring(firstNum, lastNum);
+  // Checking for negative
+  if (firstNum > 1 && eq.charAt(firstNum-1)==="-" && eq.charAt(firstNum-2)==="(" ){
+    currentValue = `-${currentValue}`;
+  }
+  const displayNum = Number(currentValue).toString();  
+  return currentValue.charAt(currentValue.length-1) === "." ? displayNum + "." : displayNum;
 }
 
 const initialState = { currentEquation: "0", displayValue: "0" };
 export const equation = (state = initialState, action) => {
-  console.log("in reducer");
-  console.timeLog(action);
   if (action.type === "EQUATION") {
-    console.log(`Here: ${action.equation}`);
     const draft = {
       currentEquation: action.equation.toString(),
       displayValue: getDisplayValue(action.equation.toString()),
